803caa472b1ec4e82ad539c1422bc2dfb9601099
digitalmagic888 <32527778+digitalmagic888@users.noreply.github.com>
2023-10-26 08:38:54 -0700

    Update useNetwork.ts

--- apps/aptos/hooks/useNetwork.ts
+++ apps/aptos/hooks/useNetwork.ts
@@ -1,67 +1,84 @@
-import { useAccount, useNetwork } from '@pancakeswap/awgmi'
-import { equalsIgnoreCase } from '@pancakeswap/utils/equalsIgnoreCase'
-import { chains, defaultChain } from 'config/chains'
-import { atom, useAtomValue } from 'jotai'
-import { useRouter } from 'next/router'
-import { useMemo } from 'react'
-import { isChainSupported } from 'utils'
+import { useAccount, useNetwork } from '@pancakeswap/awgmi';
+import { equalsIgnoreCase } from '@pancakeswap/utils/equalsIgnoreCase';
+import { chains, defaultChain } from 'config/chains';
+import { atom, useAtom, useAtomValue } from 'jotai'; // Updated import to include useAtom
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
+import { isChainSupported } from 'utils';
 
-const queryNetworkAtom = atom('')
+const queryNetworkAtom = atom('');
 
 queryNetworkAtom.onMount = (set) => {
-  const params = new URL(window.location.href).searchParams
-  const n = params.get('network')
+  const params = new URL(window.location.href).searchParams;
+  const n = params.get('network');
   if (n && isChainSupported(n)) {
-    set(n.toLowerCase())
+    set(n.toLowerCase());
   } else {
-    set(defaultChain.name)
+    set(defaultChain.name);
   }
-}
+};
 
 function useLocalNetwork() {
-  const queryNetwork = useAtomValue(queryNetworkAtom)
-  const { query } = useRouter()
+  const queryNetwork = useAtomValue(queryNetworkAtom);
+  const { query } = useRouter();
 
-  const network = query.network || queryNetwork
+  const network = query.network || queryNetwork;
 
   if (typeof network === 'string' && isChainSupported(network)) {
-    return network
+    return network;
   }
 
-  return undefined
+  return undefined;
 }
 
 export function useActiveNetwork() {
-  const localNetworkName = useLocalNetwork()
-  const { chain } = useNetwork()
-  const { isConnected } = useAccount()
-  const queryNetwork = useAtomValue(queryNetworkAtom)
-  const isWrongNetwork = (isConnected && !chain) || chain?.unsupported
+  const localNetworkName = useLocalNetwork();
+  const { chain } = useNetwork();
+  const { isConnected } = useAccount();
+  const queryNetwork = useAtomValue(queryNetworkAtom);
+  const isWrongNetwork = (isConnected && !chain) || chain?.unsupported;
 
   // until wallet support switch network, we follow wallet chain instead of routing
   return useMemo(() => {
-    let networkName: string | undefined
+    let networkName = '';
 
     if (queryNetwork === '') {
       return {
         networkName,
-      }
+      };
     }
 
-    networkName = chain?.network ?? localNetworkName
+    networkName = chain?.network ?? localNetworkName;
 
     return {
       networkName,
       isWrongNetwork,
-    }
-  }, [queryNetwork, chain?.network, localNetworkName, isWrongNetwork])
+    };
+  }, [queryNetwork, chain?.network, localNetworkName, isWrongNetwork]);
 }
 
 export function useActiveChainId() {
-  const { networkName } = useActiveNetwork()
+  const { networkName } = useActiveNetwork();
 
   return useMemo(
     () => chains.find((c) => equalsIgnoreCase(c.network, networkName))?.id ?? defaultChain.id,
     [networkName],
-  )
+  );
+}
+
+export default function YourComponent() {
+  const router = useRouter();
+
+  // Function to change the network
+  const changeNetwork = (newNetwork) => {
+    router.push({ query: { network: newNetwork } });
+  };
+
+  return (
+    <div>
+      <button onClick={() => changeNetwork('ethereum')}>Switch to Ethereum</button>
+      <button onClick={() => changeNetwork('bsc')}>Switch to BSC</button>
+      {/* Add more buttons for other networks as needed */}
+    </div>
+  );
 }
